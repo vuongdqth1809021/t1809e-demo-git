d0f5036a5500d776fabd8d8203e8798d486ca4ff
Xuan Hung <xuanhung@Xuans-MacBook-Pro.local>
2018-09-19 21:21:59 +0700

    Khoi tao project

---
+++ cmake-build-debug/CMakeFiles/t1809e.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/t1809e.dir/main.c.o"
+  "t1809e.pdb"
+  "t1809e"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/t1809e.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
